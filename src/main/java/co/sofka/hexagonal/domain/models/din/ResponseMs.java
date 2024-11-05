a722bb8cf262e9509ffd2f3588a0e7c25a34a16c
shermsofka <116570770+shermsofka@users.noreply.github.com>
2024-11-05 12:25:19 -0500

    Update ResponseMs.java

--- src/main/java/co/sofka/hexagonal/domain/models/din/ResponseMs.java
+++ src/main/java/co/sofka/hexagonal/domain/models/din/ResponseMs.java
@@ -1,4 +1,45 @@
 package co.sofka.hexagonal.domain.models.din;
 
-public class ResponseMs {
+public class ResponseMs<T> {
+    private DinHeader dinHeader;
+    private T dinBody;
+    private DinError dinError;
+
+    public ResponseMs() {
+    }
+
+    public ResponseMs(DinHeader dinHeader, T dinBody, DinError dinError) {
+        this.dinHeader = dinHeader;
+        this.dinBody = dinBody;
+        this.dinError = dinError;
+    }
+
+    public ResponseMs(DinHeader dinHeader, DinError dinError) {
+        this.dinHeader = dinHeader;
+        this.dinError = dinError;
+    }
+
+    public DinHeader getDinHeader() {
+        return dinHeader;
+    }
+
+    public void setDinHeader(DinHeader dinHeader) {
+        this.dinHeader = dinHeader;
+    }
+
+    public T getDinBody() {
+        return dinBody;
+    }
+
+    public void setDinBody(T dinBody) {
+        this.dinBody = dinBody;
+    }
+
+    public DinError getDinError() {
+        return dinError;
+    }
+
+    public void setDinError(DinError dinError) {
+        this.dinError = dinError;
+    }
 }
