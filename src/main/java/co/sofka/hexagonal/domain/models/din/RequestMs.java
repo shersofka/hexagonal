71c0da7de85bc6c3337f173105e367b5526b1015
shermsofka <116570770+shermsofka@users.noreply.github.com>
2024-11-05 12:25:16 -0500

    Update RequestMs.java

--- src/main/java/co/sofka/hexagonal/domain/models/din/RequestMs.java
+++ src/main/java/co/sofka/hexagonal/domain/models/din/RequestMs.java
@@ -2,4 +2,30 @@ package co.sofka.hexagonal.domain.models.din;
 
 public class RequestMs<T> {
 
+    private DinHeader dinHeader;
+    private T dinBody;
+
+    public RequestMs() {
+    }
+
+    public RequestMs(DinHeader dinHeader, T dinBody) {
+        this.dinHeader = dinHeader;
+        this.dinBody = dinBody;
+    }
+
+    public DinHeader getDinHeader() {
+        return dinHeader;
+    }
+
+    public void setDinHeader(DinHeader dinHeader) {
+        this.dinHeader = dinHeader;
+    }
+
+    public T getDinBody() {
+        return dinBody;
+    }
+
+    public void setDinBody(T dinBody) {
+        this.dinBody = dinBody;
+    }
 }
